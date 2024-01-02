0724a53c1d8cac3ae99ca922b49fc4bddc05b4a6
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-02 13:06:52 -0600

    set up get, post, and delete requests

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var todoController = require('./controllers/todocontroller');
 var app = express();
 //set up template engine
 app.set('view engine', 'ejs')
 
 //middleware, static files
 app.use(express.static('./public'))
+
+//fire controllers
+todoController(app);
+
 
 //localhost
 localhost:300/assets/styles.css
---
+++ public/assets/controllers/todocontroller.js
@@ -0,0 +1,16 @@
+module.exports = function(app){
+//get request
+    app.get('/todo', function(req, res){
+        
+    });
+};
+//post request
+app.post('/todo', function(req, res){
+        
+});
+
+//delete request
+
+app.delete('/todo', function(req, res){
+        
+});
